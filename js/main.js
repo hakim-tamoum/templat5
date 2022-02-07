19022fe78bceb7dba358d5a16dc031a82fdf40ea
tamoum.hakim@gmail.com <tamoum.hakim@gmail.com>
2022-02-07 00:05:56 -0500

    ajouter nicescroll

--- js/main.js
+++ js/main.js
@@ -1,8 +1,20 @@
-window.onload = function(){
+
+    // landing page
     let hdr = document.querySelector('.header');
-    let img = ['img1.jpg', 'img2.jpg', 'img3.jpg', 'img4.jpg'];
+    let img = ['img1.jpg', 'img2.jpg', 'img3.jpg', 'img4.jpg', 'img4.jpg'];
     setInterval( ()=> {
         let nbr = Math.floor(Math.random()*img.length);
-        hdr.style.backgroundImage = `url('../images/${img[nbr]}')`;
+        hdr.style.backgroundImage = `url("../img/${img[nbr]}")`;
     }, 4000);
-}
+
+    //scroll plugin
+    $("html").niceScroll(
+        {autohidemode: false,cursorwidth: '10px',cursorcolor: "#067f8a",cursorborder: "1px solid #fff",zindex: 999,
+        });
+        
+        /* $(document).ready(function() {
+            $("html").niceScroll({cursorwidth: '10px', autohidemode: false, zindex: 999, cursorcolor:"#067f8a" });
+        });*/
+
+
+
